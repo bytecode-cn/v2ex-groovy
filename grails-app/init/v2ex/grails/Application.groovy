b4907a64cfeff010d150a3497c801d4088f112d8
Echo Wang <dongxu@apache.org>
2018-03-18 23:01:04 +0800

    Init commit.

---
+++ grails-app/init/v2ex/grails/Application.groovy
@@ -0,0 +1,10 @@
+package v2ex.grails
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/v2ex/grails/BootStrap.groovy
@@ -0,0 +1,9 @@
+package v2ex.grails
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
